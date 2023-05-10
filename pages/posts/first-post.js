c5e83152c3859fa776b717c082597f0c65fe8ae4
h3xis9 <h3xis9@gmail.com>
2023-05-11 01:33:27 +0900

    Link component(アプリ内のリンク用コンポーネント)
    Link componentは、アプリ内のクライアントサイドナビゲーション(遷移)でJSでのページ遷移。通常のブラウザの遷移より速い

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,17 @@
+import Link from 'next/link';
+
 /**
  * /posts/first-post
  * 
  * @returns 
  */
 export default function FirstPost() {
-    return <h1>First Post</h1>;
+    return (
+        <>
+            <h1>First Post</h1>
+            <h2>
+                <Link href="/">Back to home</Link>
+            </h2>
+        </>
+    );
 }
